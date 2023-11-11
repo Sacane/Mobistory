261e7e471188b1a14ee6af6d4280accf1a55ffd3
JavaProgramingExpert <unconfigured@null.spigotmc.org>
2023-11-11 18:47:30 +0100

    Closing keyboard when searching

--- app/src/main/java/fr/pentagon/android/mobistory/SearchBar.kt
+++ app/src/main/java/fr/pentagon/android/mobistory/SearchBar.kt
@@ -15,7 +15,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
@@ -23,9 +25,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.pentagon.android.mobistory.ui.theme.MobistoryTheme
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchBarComponent(modifier: Modifier = Modifier, componentHeight: Int = 50) {
     var searchText by remember { mutableStateOf(TextFieldValue()) }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Row(modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
 
@@ -36,6 +40,7 @@ fun SearchBarComponent(modifier: Modifier = Modifier, componentHeight: Int = 50)
             label = { Text("Search...") },
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
             keyboardActions = KeyboardActions(onSearch = {
+                keyboardController?.hide()
                 //TODO
             })
         )
