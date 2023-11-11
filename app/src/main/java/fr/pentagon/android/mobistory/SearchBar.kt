c5325c3a2ed55a38f452f14dc493ceaae0929233
JavaProgramingExpert <unconfigured@null.spigotmc.org>
2023-11-11 18:21:44 +0100

    Changing buttons for icons

--- app/src/main/java/fr/pentagon/android/mobistory/SearchBar.kt
+++ app/src/main/java/fr/pentagon/android/mobistory/SearchBar.kt
@@ -1,10 +1,11 @@
 package fr.pentagon.android.mobistory
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -14,45 +15,49 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.pentagon.android.mobistory.ui.theme.MobistoryTheme
 
 @Composable
 fun SearchBarComponent(modifier: Modifier = Modifier) {
     var searchText by remember { mutableStateOf(TextFieldValue()) }
 
+    val iconSize = 40.dp
+
     Row(modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
 
-        Button(onClick = {
-            //action
-        }) {
-            Text("S")
-        }
+        Image(
+            painter = painterResource(id = R.drawable.magnifying_glass),
+            contentDescription = "Search icon",
+            modifier = Modifier.size(width = iconSize, height = iconSize)
+        )
 
         TextField(
             modifier = Modifier.weight(1f),
             value = searchText,
             onValueChange = { searchText = it },
-            label = { Text("Rechercher...") },
+            label = { Text("Search...") },
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
             keyboardActions = KeyboardActions(onSearch = {
                 //action
             })
         )
 
-        Button(onClick = {
-            //action
-        }) {
-            Text("F")
-        }
+        Image(
+            painter = painterResource(id = R.drawable.filter),
+            contentDescription = "Filter",
+            modifier = Modifier.size(width = iconSize, height = iconSize)
+        )
 
-        Button(onClick = {
-            //action
-        }) {
-            Text("V")
-        }
+        Image(
+            painter = painterResource(id = R.drawable.eye),
+            contentDescription = "Change view",
+            modifier = Modifier.size(width = iconSize, height = iconSize)
+        )
     }
 }
 
