647568c2a56eb0e12f0b491eb7d87791350b1010
Sacane <johan.ramaroson@gmail.com>
2023-11-12 18:47:04 +0100

    test join event And image OK

--- app/src/androidTest/java/fr/pentagon/android/mobistory/backend/entity/ImageTest.kt
+++ app/src/androidTest/java/fr/pentagon/android/mobistory/backend/entity/ImageTest.kt
@@ -6,11 +6,18 @@ import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import fr.pentagon.android.mobistory.backend.Database
+import fr.pentagon.android.mobistory.backend.Event
+import fr.pentagon.android.mobistory.backend.EventDao
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNull
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.time.Instant
+import java.util.Date
 import java.util.UUID
 
 @RunWith(AndroidJUnit4::class)
@@ -18,6 +25,7 @@ class ImageTest {
     private lateinit var db: Database
     private lateinit var imageDao: ImageDao
     private lateinit var eventImageJoin: EventImageJoinDao
+    private lateinit var eventDao: EventDao
 
     companion object{
         val URI_TEMPLATE: Uri = Uri.parse("https://localhost:8088/image.png")
@@ -32,6 +40,7 @@ class ImageTest {
         ).build()
         imageDao = db.imageDao()
         eventImageJoin = db.eventImageJoinDao()
+        eventDao = db.eventDao()
     }
 
     @Test
@@ -43,9 +52,38 @@ class ImageTest {
         assertEquals(image, imageInDb)
     }
 
-    fun insertImageWithEventTest()  = runTest {
+    @Test
+    fun imageCanBeInsertInAssociationWithEvents()  = runTest {
+        // Image
         val idImage = UUID.randomUUID()
         val image = Image(imageId = idImage, link = URI_TEMPLATE)
+
+        val idImage2 = UUID.randomUUID()
+        val image2 = Image(imageId = idImage2, link = Uri.parse("https://localhost:8088/image2.png"))
+        // Event
+        val eventId = UUID.randomUUID()
+        val event = Event(eventId = eventId, label = "Hello", startDate =  Date.from(Instant.now()), endDate = Date.from(
+            Instant.now().minusSeconds(403820)), wikipedia = "I don't know")
+
+        // Creation of Join for last created image and event
+        val join = EventImageJoin(eventId, idImage)
+        val join2 = EventImageJoin(eventId, idImage2)
+        // Save the relation in database
         imageDao.insertImage(image)
+        imageDao.insertImage(image2)
+        eventDao.save(event)
+        eventImageJoin.save(join)
+        eventImageJoin.save(join2)
+
+
+        // check the relation
+
+        val eventWithImage = eventDao.findEventWithImageById(eventId)
+
+        assertNotNull(eventWithImage)
+        assertEquals(eventWithImage!!.event, event)
+        assertTrue(image in eventWithImage.images)
+        assertTrue(image2 in eventWithImage.images)
+
     }
 }
