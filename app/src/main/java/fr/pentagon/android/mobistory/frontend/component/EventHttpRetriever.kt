132f7952a0be59cfa9f877dac57718a62e126ded
Sacane <johan.ramaroson@gmail.com>
2024-02-25 13:45:24 +0100

    Rename function

--- app/src/main/java/fr/pentagon/android/mobistory/frontend/component/EventHttpRetriever.kt
+++ app/src/main/java/fr/pentagon/android/mobistory/frontend/component/EventHttpRetriever.kt
@@ -2,13 +2,34 @@ package fr.pentagon.android.mobistory.frontend.component
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.jsoup.Jsoup
 
+@Composable
+fun EventDetail(label: String) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column {
 
-fun retrieveFromLabel(context: Context, url: String) {
+        }
+    }
+}
+
+fun findContentPageFromUrl(
+    context: Context,
+    url: String,
+    onRetrieve: (String) -> Unit = { content ->
+    Log.i(
+        "Content",
+        content
+    )
+}) {
     val queue = Volley.newRequestQueue(context)
     val stringRequest = StringRequest(
         Request.Method.GET,
@@ -16,7 +37,9 @@ fun retrieveFromLabel(context: Context, url: String) {
             val document = Jsoup.parse(response)
             val mainContentDiv = document.select("div.mw-body-content")
             if(mainContentDiv.isNotEmpty()) {
-                mainContentDiv.first()?.let { Log.i("final content", it.text()) }
+                mainContentDiv.first()?.let {
+                    onRetrieve(it.text())
+                }
             } else {
                 Log.w("NO CONTENT", "There is not final content...")
             }
