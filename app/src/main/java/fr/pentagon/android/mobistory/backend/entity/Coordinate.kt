219380602c5c17feda8831f7f059b2cfce6cc408
Sekelenao <Sekelenao@protonmail.com>
2024-03-30 23:40:48 +0100

    Modified schema

--- app/src/main/java/fr/pentagon/android/mobistory/backend/entity/Coordinate.kt
+++ app/src/main/java/fr/pentagon/android/mobistory/backend/entity/Coordinate.kt
@@ -6,12 +6,14 @@ import androidx.room.Entity
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.ABORT
 import androidx.room.PrimaryKey
+import androidx.room.Query
 import androidx.room.Relation
 import java.util.UUID
 
 @Entity(tableName = "coordinate")
 data class Coordinate(
-    val value: String,
+    val latitude : Double,
+    val longitude: Double,
     val eventId: Int,
     @PrimaryKey val coordinateId: UUID = UUID.randomUUID()
 )
@@ -23,6 +25,16 @@ interface CoordinateDao {
 
     @Insert(onConflict = ABORT)
     suspend fun saveAll(coordinates: Iterable<Coordinate>)
+
+    @Query("""
+    SELECT * FROM event INNER JOIN coordinate WHERE
+    6371 * ACOS(
+        COS(RADIANS(coordinate.latitude)) * COS(RADIANS(:latitude)) *
+        COS(RADIANS(:longitude) - RADIANS(coordinate.longitude)) +
+        SIN(RADIANS(coordinate.latitude)) * SIN(RADIANS(:latitude))
+    ) <= 15;
+    """)
+    suspend fun nearbyEventFrom(latitude: Double, longitude: Double): List<Event>
 }
 
 data class EventWithCoordinate(
